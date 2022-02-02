217eb839e2ed0642a282232cab28e937cf8ad981
kaibalyasahoo828008 <81743671+kaibalyasahoo828008@users.noreply.github.com>
2022-02-02 23:06:20 +0530

    Update character array all.cpp

--- character array all.cpp
+++ character array all.cpp
@@ -10,13 +10,25 @@ using namespace std;
 
 int main()
 {
+  char a[100];
+    char b[100]={'a','b','c'};//wrong
+    char c[100]={'a','b','c','\0'};//right
+    char d[100]="abc";
+     cout<<b<<"wrong"<<endl;
+    cout<<c<<"right"<<endl;
+     cout<<d<<"right"<<endl;
+    
+    
+    
+    
+    
     int num;
     cin>>num;
     char s[10];
     char y[num];
-    cin>>s;
+    cin>>s;//it can not take hello world and only show hello because it can not read space
     cin.ignore();
-    cin.get(s,10);
+    cin.get(s,10);//it will read one charater with spaces but it can use two times
     cin.ignore();/* you have to use it to accespt every  keya*/
     cin.getline(y,num);
     cout<<s<<endl;
@@ -26,8 +38,14 @@ int main()
     cout<<"function usig cstring string"<<endl;
     
     char s1[10]="hello";
-    cout<<strlen(s1)<<endl;/* to show the length of a an array*/
+    cout<<strlen(s1)<<endl;/* to show the length of a an array or number of characters*/
     char s2[20]="gandu";
+    
+    char s8[]="ahahhahhah";
+    cout<<sizeof(s8)<<endl;//it will add null charater extra with all charater
+    
+    
+    
     
     strcat(s1,s2);/* it will used the sourse string s2 attached to the end of destination*/
     cout<<s1<<endl;
@@ -50,11 +68,24 @@ int main()
     char s8[10]="kanpur";
     cout<<strcmp(s1,s8)<<endl;/* to compare the two string */
     
+    while(s!=NULL)
+    {
+      function are operater here;  
+        
+    }
+    while(s!='\n')
+    {
+        functions are operate here;
+    }
     
     
+    //important use of cin.get()
     
-    
-    
+    char temp=cin.get();
+    while(temp!='#')//it will not stop when you not end the string eit"#" this symbol
+    {
+        cout<<temp;
+    }
     
     
     
