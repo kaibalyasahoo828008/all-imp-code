6eeea53bd0e8dd2b77a5fbfe491f32834ad45f4c
kaibalyasahoo828008 <81743671+kaibalyasahoo828008@users.noreply.github.com>
2022-02-28 22:21:25 +0530

    Create inflix_to_proflix.cpp

---
+++ inflix_to_proflix.cpp
@@ -0,0 +1,185 @@
+
+//
+//  main.cpp
+//  450
+//
+//  Created by KAIBALYA SAHOO on 20/03/2021.
+//
+
+#include <iostream>
+#include<string.h>
+#include <stdlib.h>
+using namespace std;
+struct Node
+{
+    char data;//to store the data in node
+    struct Node *next;//to stor the adress of next node
+    
+} *top=NULL;
+void push(char x)
+{
+   struct Node *t=new Node;
+    if(t==NULL)//to check either the stack is empy or full
+    {
+        cout<<"the stack is full"<<endl;
+    }
+    else
+    {
+        t->data=x;//to insert the value in the data possition
+        t->next=top;//to move the pointer
+        top=t;
+    }
+}
+
+int pop()//for deleting the value
+{
+    int x=-1;
+   struct  Node *t;
+    if(top==NULL)
+    {
+        
+    }
+    else
+    {
+        t=top;
+        top=top->next;
+        x=t->data;
+        delete t;
+    }
+    return x;
+}
+
+void dispay()//to display the data
+{
+    struct Node *t=top;
+    while(t!=NULL)
+    {
+        cout<<t->data;
+        t=t->next;
+    }
+}
+
+/*    ******************************************************************************************/
+int isBalanced(char *exp)
+{
+    int i;
+    for(i=0;exp[i]!='\0';i++)
+    {
+        if(exp[i]=='(')
+        {
+            push(exp[i]);
+        }
+        else if(exp[i]==')')
+        {
+            if(top==NULL)
+            {
+                return 0;
+            }
+                else
+                {
+            pop();
+                }
+        }
+        
+    
+      
+    }
+         
+        if(top==NULL)
+        {
+            return 1;
+        }
+         else
+         {
+             return 0;
+         }
+    
+}
+/* *********************************************************************************************/
+int pre(char x)
+{
+    if(x=='+' || x=='-')
+    {
+        return 1;
+    }
+    else if(x=='*' || x=='/')
+    {
+            return 2;
+    }
+    else
+    {
+            return 0;
+    }
+}
+/* *********************************************************************************************/
+int isOperand(char x)
+{
+    
+    if(x=='+'|| x=='-'|| x=='*' || x=='/')
+    {
+        return 0;
+        
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+/****************************************************************************************************************************************************/
+
+char * InTopost(char *inflix)
+{
+
+    char *postflix=new char[strlen(inflix)+2];
+    int i=0,j=0;
+    while(inflix[i]!='\0')
+    {
+        if(isOperand(inflix[i]))
+        {
+           postflix[j++]=inflix[i++];
+        }
+          
+    else
+    {
+        if(pre(inflix[i])>pre(top->data))
+        {
+            push(inflix[i++]);
+        }
+        else
+        {
+            postflix[j++]=pop();
+            
+        }
+            
+            
+        }
+        
+    }
+
+while(top!=NULL)
+{
+    postflix[j++]=pop();
+}
+    
+    postflix[j]='\0';
+    
+    
+    return postflix;
+    
+}
+
+
+
+
+int main()
+{
+    char *inflix="a+b*c/e";
+    push('#');
+    char *postflix=InTopost(inflix);
+    
+    cout<<postflix;
+    
+}
+
+
